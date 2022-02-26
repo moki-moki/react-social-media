46639259bb85c3fd8b94b7cb3101d22dfe331c62
moki-moki <scrapironscarecrowdumdum@gmail.com>
2022-02-26 12:23:49 +0100

    chaning uris

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useCreateIndex: true,
+    useCreateIndex: true,
     // useFindAndModify: true,
   });
 
